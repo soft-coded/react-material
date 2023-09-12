2d72c56b6783274b32ff15637d3fb35363289927
shrutanten <shrutanten.work@gmail.com>
2023-09-12 15:43:40 +0530

    Add register form

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { TextField, Button } from "@mui/material";
 
 const Register = () => {
-    return (
-        <div>
-            <h1>Register Component</h1>
-        </div>
-    );
-}
+	return (
+		<div
+			style={{
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+				flexDirection: "column"
+			}}
+		>
+			<form
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					gap: "2rem",
+					marginTop: "1rem",
+					backgroundColor: "rgb(241, 241, 241)",
+					padding: 20,
+					borderRadius: 10
+				}}
+			>
+				<h3 style={{ textAlign: "center" }}>Register</h3>
+				<TextField label="Name"></TextField>
+				<TextField label="Email" type="email"></TextField>
+				<TextField label="Address"></TextField>
+				<TextField label="Contact number"></TextField>
+				<TextField label="Password" type="password"></TextField>
+				<Button>Submit</Button>
+			</form>
+		</div>
+	);
+};
 
 export default Register;
